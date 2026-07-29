41e5b83e1cf5c80e5eab530f3100c21423d9f47d
Alireza Esp <alireza.esp07@gmail.com>
2026-07-29 13:38:51 +0330

    add login and register page, user controller

---
+++ App/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php 
+namespace App\Controllers;
+
+use Framework\Database;
+use Framework\Validation;
+
+
+class UserController {
+    protected $db;
+
+    public function __construct() {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    public function login(): void {
+        loadView('users/login');
+    }
+
+    public function create(): void {
+        loadView('users/create');
+    }
+}
+
