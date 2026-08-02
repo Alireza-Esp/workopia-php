85a55d360c731fe20a2a9dbdd9a4a2ab2b14049f
Alireza Esp <alireza.esp07@gmail.com>
2026-08-02 14:08:13 +0330

    add logout functionality

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -105,6 +105,15 @@ class UserController {
 
         redirect('/');
     }
-    
+
+    public function logout(): void {
+        Session::clearAll();
+
+        $params = session_get_cookie_params();
+
+        setcookie('PHPSESSID', '', time() - 86400, $params['path'], $params['domain']);
+
+        redirect('/');
+    }
 }
 
--- App/views/partials/header.php
+++ App/views/partials/header.php
@@ -9,16 +9,22 @@ use Framework\Session;
             <a href="/">Workopia</a>
         </h1>
         <nav class="space-x-4">
-            <?php if(Session::exists('user')): ?>
-                <a href="#" class="text-white">Welcome <?= Session::get('user')['name'] ?></a>
-                <a href="/auth/logout" class="text-white hover:underline">Logout</a>
-                <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
-                    <i class="fa fa-edit"></i> Post a Job
-                </a>
-            <?php else: ?>
-                <a href="/auth/login" class="text-white hover:underline">Login</a>
-                <a href="/auth/register" class="text-white hover:underline">Register</a>
-            <?php endif?>
+            <div class="flex justify-between items-center gap-4">
+                <?php if(Session::exists('user')): ?>
+                    <p class="text-white">Welcome <?= Session::get('user')['name'] ?></p>
+                    <form method="POST" action="/auth/logout">
+                        <button type="submit" class="text-white inline hover:underline">
+                            Logout
+                        </button>
+                    </form>
+                    <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
+                        <i class="fa fa-edit"></i> Post a Job
+                    </a>
+                <?php else: ?>
+                    <a href="/auth/login" class="text-white hover:underline">Login</a>
+                    <a href="/auth/register" class="text-white hover:underline">Register</a>
+                <?php endif?>
+            </div>
         </nav>
     </div>
 </header>
